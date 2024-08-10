c00ae805110f5748a4b2e12db417ecb1d2797369
PB <xachineon@gmail.com>
2024-08-10 13:08:52 +0530

    first push

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-function max(num1,num2)
+function findmax(num1,num2)
 {
     return num1>num2?num1:num2
 }
-
-console.log('The larger of the 2 numbers is '+max(3,1));
+console.log(
+                'The larger of the 2 numbers is '
+                +findmax(3,1)
+            );
